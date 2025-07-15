b44d9f297510381aca158143b17b750c2431b79b
TOBIH12 <ayomikunbolaji43@gmail.com>
2025-07-14 12:08:12 -1200

    frontend replacement

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://blog-website-frontend-ok5g.vercel.app'); // Replace with your frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({origin: 'http://localhost:5173', credentials: true})); // Allow CORS for the frontend app
+app.use(cors({origin: 'https://blog-website-frontend-ok5g.vercel.app', credentials: true})); // Allow CORS for the frontend app
 app.use(upload());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
