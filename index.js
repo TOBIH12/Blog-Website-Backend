a3dfd8a31645ae36c80aba9f625b9979988feab3
TOBIH12 <ayomikunbolaji43@gmail.com>
2025-09-28 21:22:53 +0100

    newer updates

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://blog-website-frontend-ok5g.vercel.app/'); // Replace with your frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
